740d473ca8c804e0a30a13055fd97538b5437130
suyog prasai <suyog224356@gmail.com>
2025-11-12 05:20:52 +0000

    added home page and models

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "192.168.1.136",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/types/models/podcast.ts
@@ -0,0 +1,21 @@
+
+export type PodcastType = "story" | "legend" | "entity";
+
+export interface podcast {
+    title?: string;
+    name?: string;
+    content?: string;
+    synopsis: string;
+    alt_name?: string;
+    author: string;
+    picture: string;
+    pic?: string;
+    uuid: string;
+    likes: number;
+    entity?: podcast[];
+    legends?: podcast[];
+    stories?: podcast[];
+    type: PodcastType;
+    audio?: string;
+    user_like: boolean;
+}
