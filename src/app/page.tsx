e1eae0cfaabea49b433cc22e895b3e3715f58c80
suyog prasai <suyog224356@gmail.com>
2025-11-12 05:12:32 +0000

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen w-full bg-primary-background text-secondary-text flex flex-col justify-center items-center">
 
+      <h1 className="text-4xl font-bold mb-3 font-bebas-neue">Bakhabazar</h1>
+      <p className="text-primary-text mb-6 text-center">
+        AI powered podcast experience
+      </p>
+
+      <a
+        href="/home"
+        className="bg-secondary-button-background cursor-pointer text-secondary-button-foreground px-6 py-2 rounded-md font-medium hover:bg-gray-200 transition">
+
+        Go to Home
+      </a>
 
 
     </div>
