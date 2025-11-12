c9970f416a99e13830cc55e29666efb704f5cb7c
suyog prasai <suyog224356@gmail.com>
2025-11-12 08:33:26 +0000

    added packages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
+  output: "standalone",
+  allowedDevOrigins: ["http://192.168.1.136"],
 
+  images: {
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
       {
         protocol: "http",
         hostname: "192.168.1.136",
-      }
-    ]
-  }
+        pathname: "/media/**", // important to allow your media directory
+      },
+      {
+        protocol: "http",
+        hostname: "frontend", // typo fixed from 'frotnend'
+      },
+      {
+        protocol: "http",
+        hostname: "0.0.0.0",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
